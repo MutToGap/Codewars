f13c4f248ffd1f5be29e1c3828ad535b591716ad
MutToGap <muttogap@gmail.com>
2018-08-14 20:34:27 +0200

    Add DuplicateEncode Kata | 6 Kyu

---
+++ C#/DuplicateEncode.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * 6 Kyu - Duplicate Encoder
+ * https://www.codewars.com/kata/54b42f9314d9229fd6000d9c
+ */
+
+using System;
+using System.Linq;
+
+public class Kata {
+    public static string DuplicateEncode(string word) {
+        string result = "";
+        word = word.ToLower();
+        foreach (Char c in word) {
+            result += (word.Count(f => f == c) > 1 ? ")" : "(");
+        }
+        return result;
+    }
+}
