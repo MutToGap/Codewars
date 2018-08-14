90f70093791b2c1997f5edd40a2318bae8df7ea7
MutToGap <muttogap@gmail.com>
2018-08-14 20:08:34 +0200

    Add SquareDigits Kata | 7 Kyu

---
+++ C#/SquareDigits.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * 7 Kyu - Square Every Digit
+ * https://www.codewars.com/kata/546e2562b03326a88e000020
+ */
+
+using System;
+using System.Linq;
+
+public static class Kata {
+    public static int SquareDigits(int n) {
+        string result = "";
+        n.ToString().ToList().ForEach(x => result += Math.Pow(Int32.Parse(x.ToString()), 2).ToString());
+        return Int32.Parse(result);
+    }
+}
