4d5edc982b5904d937b8050113d36cafe6e4ab92
MutToGap <muttogap@gmail.com>
2018-08-14 20:39:50 +0200

    Add Order Kata | 6 Kyu

---
+++ C#/Order.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * 6 Kyu - Your order, please
+ * https://www.codewars.com/kata/55c45be3b2079eccff00010f
+ */
+
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Kata {
+    public static string Order(string words) {
+        string result = "";
+        List<string> split = words.Split(' ').ToList();
+        for (int n = 0; n < split.Count; n++) {
+            for (int m = 0; m < split.Count; m++) {
+                if (split[m].Contains((n + 1).ToString())) {
+                    result += split[m];
+                    break;
+                }
+            }
+            result += (n + 1 == split.Count) ? "" : " ";
+        }
+        return result;
+    }
+}
