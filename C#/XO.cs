4e5d357bdb897636567a2fd32ce6de77d41f9bc8
MutToGap <muttogap@gmail.com>
2018-08-14 19:55:31 +0200

    Add XO Kata | 7 kyu

---
+++ C#/XO.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * 7 Kyu - Exes and Ohs
+ * https://www.codewars.com/kata/55908aad6620c066bc00002a
+ */
+
+using System.Linq;
+
+namespace Codewars {
+    public static class Kata {
+        public static bool XO(string input) {
+            int oCount = input.Count(n => n == 'o' || n == 'O');
+            int xCount = input.Count(n => n == 'x' || n == 'X');
+            return oCount == xCount;
+        }
+    }
+}
