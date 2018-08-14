57fd1e97711050c36cc6c10b0628f10ecb5768ca
MutToGap <muttogap@gmail.com>
2018-08-14 20:30:52 +0200

    Add Find Kata | 6 Kyu

---
+++ C#/Find.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * 6 Kyu - Find The Parity Outlier
+ * https://www.codewars.com/kata/5526fc09a1bbd946250002dc
+ */
+
+using System.Collections.Generic;
+
+public class Kata {
+    public static int Find(int[] integers) {
+        List<int> odd = new List<int>(), even = new List<int>();
+        foreach (int n in integers) {
+            ((n % 2 == 0) ? even : odd).Add(n);
+        }
+        return odd.Count < even.Count ? odd[0] : even[0];
+    }
+}
