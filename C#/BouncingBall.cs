1efb8f86abbdfe8c2da55ae7308b6659a538e0f1
MutToGap <muttogap@gmail.com>
2018-08-14 21:16:44 +0200

    Add BouncingBall | 6 Kyu

---
+++ C#/BouncingBall.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * 6 Kyu - Bouncing Balls
+ * https://www.codewars.com/kata/5544c7a5cb454edb3c000047
+ */
+
+public class BouncingBall {
+    public static int bouncingBall(double h, double bounce, double window) {
+        if (h > 0 && bounce < 1 && bounce > 0 && window < h) {
+            int count = 1;
+            while (h * bounce > window) {
+                h *= bounce;
+                count += 2;
+            }
+            return count;
+        } else {
+            return -1;
+        }
+    }
+}
