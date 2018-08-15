a06dc4465259a4c3a34a07016287079a7b75caf2
MutToGap <muttogap@gmail.com>
2018-08-15 17:05:13 +0200

    Add IsPangram | 6 Kyu

---
+++ C#/IsPangram.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * 6 Kyu - Detect Pangram
+ * https://www.codewars.com/kata/545cedaa9943f7fe7b000048
+ */
+
+using System;
+using System.Linq;
+
+public static class Kata {
+    public static bool IsPangram(string str) {
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        str = str.ToLower();
+        foreach (Char letter in alphabet) {
+            if (!str.Contains(letter)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
