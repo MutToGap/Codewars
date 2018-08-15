59d91ee37936a661d0cf8f28be2652c0b77ae682
MutToGap <muttogap@gmail.com>
2018-08-15 03:59:03 +0200

    Add RemovNb | 5 Kyu

---
+++ C#/RemovNb.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * 5 Kyu - Is my friend cheating?
+ * https://www.codewars.com/kata/5547cc7dcad755e480000004
+ */
+
+using System.Collections.Generic;
+
+public class RemovedNumbers {
+    public static List<long[]> removNb(long n) {
+        List<long[]> result = new List<long[]>();
+        long sum = n * (n + 1) / 2;
+        for (int j = 1; j <= n; j++) {
+            long a = (sum - j) / (j + 1);
+            if (a * j + a + j == sum && a < n) {
+                result.Add(new long[] { j, a });
+            }
+        }
+        return result;
+    }
+}
